b36ce2420b14c259f85afd33ef7cbdb6acb1f369
ICEBACKTECHNOLOGY <xurupibaza935@gmail.com>
2025-05-27 22:54:50 -1200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'SADBWOY-BUG',
+  author: '‎DEVO-AM-LUCIFER',
+  botName: "SADBWOY-CRASHER",
+  botOwner: 'ERROR-ICEBACK-N-ALPHA_KING', // Your name
+  ownerNumber: '263718728504', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
